c08cab48c0dddaffe0db30e3e7d4d8175830e353
Fatima Sohail Shaukat <82968880+Fatim-Sohail@users.noreply.github.com>
2023-12-09 08:20:09 +0500

    Time: 123 ms (97.94%), Space: 21.1 MB (70.43%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        if not intervals:
+            return []
+
+        # Step 1: Sort intervals based on their start times
+        intervals.sort(key=lambda x: x[0])
+
+        # Step 2: Merge overlapping intervals
+        merged_intervals = [intervals[0]]
+        for interval in intervals[1:]:
+            if interval[0] <= merged_intervals[-1][1]:
+                # Merge overlapping intervals
+                merged_intervals[-1][1] = max(merged_intervals[-1][1], interval[1])
+            else:
+                # Add non-overlapping interval to the result
+                merged_intervals.append(interval)
+
+        return merged_intervals
